fecf1bf5b1848a8d1dedab5ed448f0a73c25fc19
iv571 <ivelj571@mtroyal.ca>
2024-03-08 16:11:31 -0700

    prolog queries complete

--- companyext.pl
+++ companyext.pl
@@ -8,8 +8,7 @@ employee(ramesh).
 employee(joyce).
 employee(ahmad).
 employee(james).
-employee(joe).
-employee(jonas).
+
 
 salary(john, 30000).
 salary(franklin, 40001).
@@ -19,8 +18,7 @@ salary(ramesh, 38000).
 salary(joyce, 25000).
 salary(ahmad, 25000).
 salary(james, 55000).
-salary(joe, 29000).
-salary(jonas, 35000).
+
 
 department(john, research).
 department(franklin, research).
@@ -30,9 +28,7 @@ department(ramesh, research).
 department(joyce, research).
 department(ahmad, administration).
 department(james, headquarters).
-department(joe, administration).
-department(jane, administration).
-department(jonas, administration).
+
 
 supervise(franklin, john).
 supervise(franklin, ramesh).
@@ -41,9 +37,7 @@ supervise(jennifer, alicia).
 supervise(jennifer, ahmad).
 supervise(james, franklin).
 supervise(james, jennifer).
-supervise(ahmad, joe).
-supervise(ahmad, jane).
-supervise(ahmad, jonas).
+
 
 female(alicia).
 female(jennifer).
@@ -55,8 +49,7 @@ male(franklin).
 male(ramesh).
 male(ahmad).
 male(james).
-male(joe).
-male(jonas).
+
 
 project(productx).
 project(producty).
@@ -81,14 +74,7 @@ works_on(ahmad, newbenefits, 5).
 works_on(jennifer, newbenefits, 20).
 works_on(jennifer, reorganization, 15).
 works_on(james, reorganization, 10).
-works_on(joe, reorganization, 30).
-works_on(joe, newbenefits, 10).
-works_on(jane, reorganization, 20).
-works_on(jane, producty, 10).
-works_on(jane, productx, 10).
-works_on(jonas, productz, 20).
-works_on(jonas, productx, 10).
-works_on(jonas, producty, 10).
+
 
 %-------------------------------------------------------------
 % Rules
@@ -98,6 +84,7 @@ subordinate(X,Y) :- superior(Y,X).
 
 % Should you need to write new rules, please enter them here.
 
+supreme_chief(SupremeChief) :- supervise(SupremeChief, _), supervise(_, SupremeChief).
 
 %-------------------------------------------------------------
 % Queries
@@ -107,23 +94,21 @@ subordinate(X,Y) :- superior(Y,X).
  "computerization" with an effort of "10" hours per week, and that have
  "jennifer" as a supervisor? If so, list them.
 
-female(Employee), works_on(Employee, computerization, 10), supervise(jennifer, Employee).
-% result is Alicia
+female(Employee), works_on(Employee, computerization, 10), supervise(jennifer, Employee). % result is Alicia
 
 % Q2: Who is the employee who makes over "40000" dollars a year and works
 for the "research" department?
 
-salary(Employee, Amount), Amount > 40000, department(Employee, research).
-% The Employee is Franklin
+salary(Employee, Amount), Amount > 40000, department(Employee, research). % The Employee is Franklin
 
 % Q3: Who is the supreme chief of this fictional company (aka the President)
 
-
+supreme_chief(SupremeChief). % Franklin is the Supreme Chief
 
 % Q4: Who are the individuals that work on project "productx" with an *effort*
 of 20 or more hours a week?
 
-
+works_on(Individual, productx, Hours), Hours >= 20. % John works on productx more than 20 hours a week
 
 
 
